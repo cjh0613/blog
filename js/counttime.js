0c3887129e04a04afc9cb4c289a8dbb22028eaa2
cjh0613 <cjhbruce@foxmail.com>
2020-11-03 10:59:18 +0800

    更新网站 2020-11-03 10:59

--- js/counttime.js
+++ js/counttime.js
@@ -23,5 +23,7 @@ var $runtimeCount = $('#webinfo-runtime-count')
     showDateTime()
     clearInterval(interval)
     interval = setInterval(showDateTime, 10000)
-    
+    $(".post").prepend('<div class="post-excerpt"><div class="note-plugin success no-icon"><p>如果有兴趣，可以关注一下 <strong>TreeCard</strong> ——<br>优势：世界上第一款木制借记卡，万事达卡，境外机构发行，免费。最易申请、最优惠的境外卡。<br>12.1 前可以免费申请。可以查看我写的这篇指南<a href="/20201029treecard.html">TreeCard木制万事达借记卡申请指南</a>了解更多。</p></div></div>');
+
+
     $(".reward-qrcode").attr("style","opacity: 1; display: block; transform: translateY(0px);");});
